875f06cfca51d006c29eabcad72b045c1984f213
HR-JI <jihr29@gmail.com>
2022-08-22 17:21:32 +0900

    출력 로그추가

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /*
     라쿤이 작성 js파일
 */
-console.log('app.js')
-console.log('sourcetree로 커밋')
+console.log('app.js');
+console.log('sourcetree로 커밋');
+console('amend 옵션으로 commit 수정하기');
--- board.js
+++ board.js
@@ -4,3 +4,4 @@
 
 console.log('board');
 console.log('develop 에서 추가함');
+console('amend 옵션으로 commit 수정하기');
